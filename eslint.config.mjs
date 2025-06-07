89f057e617021d29b99d3a197d3b0e3b89223a8e
Maria Gabrielly <mgabriellyanisio@gmail.com>
2025-06-07 11:35:31 -0300

    chore: eslint and prettier setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { Quote } from "lucide-react";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,20 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.config({
+    extends: ["next/core-web-vitals", "next/typescript", "prettier"],
+    rules: {
+      semi: ["error"],
+      quotes: ["error", "double"],
+      "prefer-arrow-callback": "error",
+      "no-multi-spaces": "error",
+      "no-trailing-spaces": "error",
+      "indent": ["error", 2],
+      "@typescript-eslint/no-unused-vars": ["warn"],
+      "object-curly-spacing": ["error", "always"],
+      "no-multiple-empty-lines": ["error", { max: 1, maxEOF: 0 }]
+    }
+  })
 ];
 
 export default eslintConfig;
