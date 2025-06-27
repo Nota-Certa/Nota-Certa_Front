08d9fe2efc0c56a279896afd017877736ed0706d
Lucas Venancio <lucas.venancio@outlook.com>
2025-06-27 16:30:03 -0300

    feat: back integratio

--- src/app/components/button.tsx
+++ src/app/components/button.tsx
@@ -1,14 +1,36 @@
 import { Button } from "@/components/ui/button";
 
-interface ButtonProps{
-    label: string;
-    href?: string;
+interface ButtonProps {
+  label: string;
+  href?: string;
+  onClick?: () => void;
+  disabled?: boolean;
 }
 
 export function ButtonComponent(props: ButtonProps) {
+  if (props.href) {
+    return (
+      <div className="flex flex-wrap items-center gap-2 md:flex-row">
+        <a href={props.href}>
+          <Button
+            className="bg-white text-primary-blue hover:bg-secondary-blue hover:text-white font-bold text-xl"
+            disabled={props.disabled}
+          >
+            {props.label}
+          </Button>
+        </a>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-wrap items-center gap-2 md:flex-row">
-      <a href={props.href}><Button className="bg-white text-primary-blue hover:bg-secondary-blue hover:text-white font-bold text-xl" >{props.label}</Button></a>
+      <Button
+        className="bg-white text-primary-blue hover:bg-secondary-blue hover:text-white font-bold text-xl"
+        onClick={props.onClick}
+        disabled={props.disabled}
+      >
+        {props.label}
+      </Button>
     </div>
   );
 }
--- src/app/components/inputField.tsx
+++ src/app/components/inputField.tsx
@@ -1,18 +1,21 @@
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import React from "react";
 
 interface InputFieldProps {
   type: string
   label: string,
   placeholder: string,
   classNameInput?: string,
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export function InputField(props: InputFieldProps) {
   return (
     <div className="flex flex-col gap-y-2 w-full">
       <Label htmlFor="email">{props.label}</Label>
-      <Input className={`bg-white text-black w-full ${props.classNameInput} `} type={props.type} placeholder={props.placeholder} />
+      <Input className={`bg-white text-black w-full ${props.classNameInput} `} type={props.type} placeholder={props.placeholder} value={props.value} onChange={props.onChange} />
     </div>
   );
 }
