774e0da1b33848d9865f1cfa0ee13e349dba836e
Lucas Venancio <lucas.venancio@outlook.com>
2025-07-27 14:19:08 -0300

    cadastro fix

--- src/app/(pages)/cadastro/page.tsx
+++ src/app/(pages)/cadastro/page.tsx
@@ -21,7 +21,7 @@ export default function Cadastro() {
   async function handleSubmit(){
     try {
       await cadastro(nome, empresa, telefone, email, cnpj, senha );
-      router.push("/assinatura");
+      router.push("/login");
     } catch (err) {
       setError(`Credenciais inválidas! Verifique seus dados. ${err}`);
     }
